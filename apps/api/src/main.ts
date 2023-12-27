0792861eb523f6bb4b6bbfa0960ce1ffa41d7d56
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-12-27 16:56:22 +0530

    add nx

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { add } from '@foundation/sample-lib';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('add ', add(3, 4));
   await app.listen(3000);
 }
 bootstrap();
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { add } from '@foundation/sample-lib'
+
 export default function Home() {
-  return <main>Hello world</main>
+  return <main>Hello world {add(3, 4)} </main>
 }
